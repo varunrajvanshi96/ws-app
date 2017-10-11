ba06912e59d53a635a660acc59c36d7112e36733
iamv_r <varunrajvanshi@outlook.com>
2017-10-11 17:56:02 +0530

    set up the chat box and listeners

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ var io=socket(server);
 
 io.on('connection',function(socket){
   console.log('made socket connection',socket.id);
+  socket.on('chat',function(data){
+	io.sockets.emit('chat',data);
+});
 });
