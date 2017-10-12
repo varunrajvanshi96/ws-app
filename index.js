b6c11ec60fe641c82871a3a1bd9ad56bed376642
iamv_r <varunrajvanshi@outlook.com>
2017-10-13 02:09:18 +0530

    fixed some info abous us(no coding)

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var socket = require('socket.io');
 // App setup
 var app = express();
 var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+    console.log('hi there.!');
+    console.log('your app is now running,');
+    console.log('And is now listening for requests on port 4000,');
 });
 
 // Static files
