a29a3e6d4caed53cb33bd0ee755f62d202ffa6a9
iamv_r <varunrajvanshi@outlook.com>
2017-10-12 14:15:58 +0530

    fixed styles.css in master

--- public/chat.js
+++ public/chat.js
@@ -13,7 +13,7 @@ btn.addEventListener('click', function(){
       message: message.value,
       handle: handle.value
   });
-  message.value = "";
+
 });
 
 message.addEventListener('keypress', function(){
@@ -24,6 +24,6 @@ socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 socket.on('typing', function(data){
-    feedback.innerHTML = "";
+    feedback.innerHTML = " ";
     feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
